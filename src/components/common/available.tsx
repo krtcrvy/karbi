a65e892b1d57e81ed63ec3b527fd5f8ebfbd8f10
Kurt Carvey Cadenas <kurtcarvey.m.cadenas@gmail.com>
2024-09-16 04:00:53 +0800

    Refactor Available component to use AnimatedShinyText for dynamic text

--- src/components/common/available.tsx
+++ src/components/common/available.tsx
@@ -1,10 +1,12 @@
+import AnimatedShinyText from "@/components/magicui/animated-shiny-text";
+
 export default function Available() {
   return (
     <div className="border border-[#0c0a091a] dark:border-[#fafaf91a] px-4 py-1.5 mb-8 inline-flex items-center gap-4 rounded-lg">
       <div className="bg-green-500 size-2.5 rounded-full animate-pulse" />
-      <div className="text-muted-foreground text-sm font-medium">
+      <AnimatedShinyText className="text-muted-foreground text-sm font-medium">
         Available for new projects
-      </div>
+      </AnimatedShinyText>
     </div>
   );
 }
