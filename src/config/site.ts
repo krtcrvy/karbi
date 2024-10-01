f00daba23c31e9a3bb128026c3ca0b3ba0e5e0d0
Kurt Carvey Cadenas <kurtcarvey.m.cadenas@gmail.com>
2024-10-01 18:40:50 +0800

    Update resume link in site configuration

--- src/config/site.ts
+++ src/config/site.ts
@@ -15,6 +15,6 @@ export const siteConfig: SiteConfig = {
     linkedin: "https://www.linkedin.com/in/kurtcarvey-cadenas",
     email: "kurtcarvey.m.cadenas@gmail.com",
     resume:
-      "https://drive.google.com/file/d/1UZMXwKwiQ32oMhiztvYa8070gGa-VVp7/view?usp=sharing",
+      "https://drive.google.com/file/d/16OQGu47vpXpTBhVnehUiLKlgSsWLFN-i/view?usp=sharing",
   },
 };
